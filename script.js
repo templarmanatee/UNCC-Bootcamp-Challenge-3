d0e65d71a23fc3778f615ba5980b646bcc252e78
TemplarManatee <dylanbrad10@gmail.com>
2022-10-27 01:05:40 -0400

    Basic alphanumeric arrays

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Assignment code here
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; 
+var numeric = [1,2,3,4,5,6,7,8,9,0]; 
+var specialChar = []; 
+
+function writePassword() {
+
+}
+
 
 
 // Get references to the #generate element
