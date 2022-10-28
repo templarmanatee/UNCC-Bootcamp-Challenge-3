0f013fc7c8a4b6cfd6662d3e5153b81417e11489
TemplarManatee <dylanbrad10@gmail.com>
2022-10-27 20:57:37 -0400

    Debugged input validation and string generation

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+//Arrays of valid characters
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; 
 var numeric = ['1','2','3','4','5','6','7','8','9','0']; 
 var specialChar = ['!','@','#','$','%','&','(',')','*','+','-','.','/',':',';','<','>','?'];
 
+//Array for storing selected characters
 var selectedTypes = [];
-console.log(selectedTypes);
 
-//Set up inputs for password criteria and return them as an array. 
+//Prompts the user for the chosen password length and returns that value for validation
+//Confirms the types of characters used: lowercase, uppercase, numbers, and/or special characters 
 function passCriteria() {
   var numChars = window.prompt("How long would you like your password to be?");
-  inputValidator(numChars); 
   
   var inclLower = window.confirm("Include lowercase characters?");
   var inclUpper = window.confirm("Include uppercase characters?");
@@ -17,49 +18,51 @@ function passCriteria() {
   var inclSpecial = window.confirm("Do you want special characters?");
 
   if(inclLower){
-    selectedTypes.push(lowerCase);
-    console.log(lowerCase);
+    selectedTypes = selectedTypes.concat(lowerCase);
   } 
   if(inclUpper){
-    selectedTypes.push(upperCase); 
-    console.log(upperCase);
+    selectedTypes = selectedTypes.concat(upperCase);
   }
   if(inclNum){
-    selectedTypes.push(numeric); 
+    selectedTypes = selectedTypes.concat(numeric); 
   }
   if (inclSpecial){
-    selectedTypes.push(specialChar); 
+    selectedTypes = selectedTypes.concat(specialChar); 
   }
+  console.log(selectedTypes);
   return(numChars);
 }
 
-//Input validation
+//Confirms whether or not the user entered a valid integer between 7 and 129
+//Continues to prompt the user until they enter a valid input
+//Returns a valid integer
 function inputValidator(numChars) {
   if((numChars == parseInt(numChars)) && (numChars > 7 && numChars < 129)){
     return(parseInt(numChars)); 
   } else {
     window.alert("Valid numeric character not entered. Value must be greater than 7 and less than 29 characters.");
-    inputValidator(window.prompt("Reenter the number of characters in the password: "));
+    var reenter = window.prompt("Reenter the number of characters in the password: ");
+    return(inputValidator(reenter));
   }
 }
 
-//Generate random values and pick from one of each applicable array
+//Generates random characters and concatenates them to a string
+//Returns the string once the loop has finished
 function genChar(numChars) {
   var finalString = ''; 
 
   for (i=0; i < numChars; i++) {
-    //Potential issue 1
-    finalString.push(selectedTypes[Math.floor(Math.random() * selectedTypes.length)]);
+    finalString = finalString.concat(selectedTypes[Math.floor(Math.random() * selectedTypes.length)]);
   } 
   console.log(finalString);
   return finalString; 
 }
 
-//Return final concatenated string 
-//Potential issue 2
+//Driver function
+//Prompts user for password criteria, validates the amount of characters, generates a suitable password, and returns it.
 function generatePassword() {
   var passLength = passCriteria();
-  console.log(passLength);
+  passLength = inputValidator(passLength);
   var finalPass = genChar(passLength);
   return finalPass;
 }
@@ -68,11 +71,13 @@ function generatePassword() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// Reset selectedTypes[] once the password has been generated
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  selectedTypes.length = 0; 
 }
 
 // Add event listener to generate button
